e4639989f7754503467250c6b8ebeaf63cf1ec33
milankatira <milankatira26@gmail.com>
2021-11-15 08:11:00 +0530

    prop types testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import Header from './components/header/Header';
+import Header from "./components/header/Header";
+import Headline from "./components/headline/Headline";
+import styled from "styled-components";
 function App() {
   return (
     <div>
-<Header/>
+      <Header />
+      <Wrapper>
+        <Headline header='posts' desc='click the button' />
+      </Wrapper>
     </div>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+`;
---
+++ src/components/headline/headline.test.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Headline from './Headline';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+Enzyme.configure({ adapter: new Adapter() });
+
+import { findByTestAttr, checkProps } from '../../../utils/index';
+
+const setUp = (props={}) => {
+    const component = shallow(<Headline {...props} />);
+    return component;
+};
+
+describe('Headline Component', () => {
+
+    describe('Checking PropTypes', () => {
+
+        it('Should not throw a warning', () => {
+
+            const expectedProps = {
+                header: 'Test Header',
+                desc: 'Test Desc',
+                tempArr: [{
+                    fName: 'Test fName',
+                    lName: 'Test lName',
+                    email: 'test@email.com',
+                    age: 21,
+                    onlineStatus: false
+                }]
+            };
+            const propsErr = checkProps(Headline, expectedProps)
+            expect(propsErr).toBeUndefined();
+
+        });
+
+    });
+
+    describe('Have props', () => {
+
+        let wrapper;
+        beforeEach(() => {
+            const props = {
+                header: 'Test Header',
+                desc: 'Test Desc'
+            };
+            wrapper = setUp(props);
+        });
+
+        it('Should render without errors', () => {
+            const component = findByTestAttr(wrapper, 'HeadlineComponent');
+            expect(component.length).toBe(1);
+        });
+
+        it('Should render a H1', () => {
+            const h1 = findByTestAttr(wrapper, 'header');
+            expect(h1.length).toBe(1);
+        });
+
+        it('Should render a desc', () => {
+            const desc = findByTestAttr(wrapper, 'desc');
+            expect(desc.length).toBe(1);
+        });
+
+    });
+
+    describe('Have NO props', () => {
+
+        let wrapper;
+        beforeEach(() => {
+            wrapper = setUp();
+        });
+
+        it('Should not render', () => {
+            const component = findByTestAttr(wrapper, 'HeadlineComponent');
+            expect(component.length).toBe(0);
+        });
+
+
+    });
+
+});
